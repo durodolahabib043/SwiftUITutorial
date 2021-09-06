1883691e1fb6ac9a118688f79f188fee5df79256
durodolahabib043 <durodolahabib043@gmail.com>
2021-09-06 14:14:39 -0400

    lesson 1

--- SwiftUI-Walkthrough/ContentView.swift
+++ SwiftUI-Walkthrough/ContentView.swift
@@ -10,11 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Image(systemName: "moon.stars.fill").resizable().aspectRatio(contentMode: .fit).padding(30).background(Color.green)
+    
             Text("Hello, world!")
                 .padding(5)
-            Text("here")
+                .background(Color.red.blur(radius: 3.0))
+            Button(action: show) {
+                Text("Click here").padding().foregroundColor(Color.orange)
+            }
+
+            ZStack {
+                Image(systemName: "moon.stars.fill").resizable().aspectRatio(contentMode: .fit).padding(30).background(Color.green)
+               
+                HStack(spacing: 40, content: {
+                    Text("Double")
+                    Text("Mileage")
+                }).background(Color.pink)
+            }
+            
         }
+ 
     
+    }
+    
+    fileprivate func show(){
+        print("this is clcicked")
     }
 }
 
