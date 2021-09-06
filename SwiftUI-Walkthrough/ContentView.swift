3165f4a7ae074a465a12aa61a26c7d2603e0f7f9
durodolahabib043 <durodolahabib043@gmail.com>
2021-09-06 13:00:40 -0400

    update

---
+++ SwiftUI-Walkthrough/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI-Walkthrough
+//
+//  Created by Habib Durodola on 9/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding(5)
+            Text("here")
+        }
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Walkthrough/SwiftUI_WalkthroughApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WalkthroughApp.swift
+//  SwiftUI-Walkthrough
+//
+//  Created by Habib Durodola on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WalkthroughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
